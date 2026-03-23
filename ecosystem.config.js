fd623e42793ed12b1857c4ffac9b8a69bd3779be
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2026-03-23 23:50:35 +0530

    Refactor ecosystem configuration to use relative paths for scripts and log files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,27 +2,18 @@ module.exports = {
   apps: [
     {
       name: "config-server",
-      script: "C:/Users/ZeejMaleesha/.jdks/openjdk-23.0.1/bin/java.exe",
-      args: "-jar G:/eca/platform/config-server/target/Config-Server-1.0.0.jar",
-      log_file: "G:/eca/platform/logs/config-server.log",
-
-
+      script: "java -jar ./config-server/target/Config-Server-1.0.0.jar",
+      log_file: "./logs/config-server.log",
     },
     {
       name: "service-registry",
-      script: "C:/Users/ZeejMaleesha/.jdks/openjdk-23.0.1/bin/java.exe",
-      args: "-jar G:/eca/platform/service-registry/target/Service-Registry-1.0.0.jar",
-      log_file: "G:/eca/platform/logs/service-registry.log",
-
-
+      script: "java -jar ./service-registry/target/Service-Registry-1.0.0.jar",
+      log_file: "./logs/service-registry.log",
     },
     {
       name: "api-gateway",
-      script: "C:/Users/ZeejMaleesha/.jdks/openjdk-23.0.1/bin/java.exe",
-      args: "-jar G:/eca/platform/api-gateway/target/Api-Gateway-1.0.0.jar",
-      log_file: "G:/eca/platform/logs/api-gateway.log",
-
-
+      script: "java -jar ./api-gateway/target/Api-Gateway-1.0.0.jar",
+      log_file: "./logs/api-gateway.log",
     }
   ]
 }
