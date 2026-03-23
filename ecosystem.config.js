07950e55127c3a7d1db8101d336b98de76a4edbc
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2026-03-23 23:23:36 +0530

    Add log file paths for config server, service registry, and API gateway

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,15 @@ module.exports = {
       name: "config-server",
       script: "C:/Users/ZeejMaleesha/.jdks/openjdk-23.0.1/bin/java.exe",
       args: "-jar G:/eca/platform/config-server/target/Config-Server-1.0.0.jar",
+      log_file: "G:/eca/platform/logs/config-server.log",
+
 
     },
     {
       name: "service-registry",
       script: "C:/Users/ZeejMaleesha/.jdks/openjdk-23.0.1/bin/java.exe",
       args: "-jar G:/eca/platform/service-registry/target/Service-Registry-1.0.0.jar",
+      log_file: "G:/eca/platform/logs/service-registry.log",
 
 
     },
@@ -17,6 +20,7 @@ module.exports = {
       name: "api-gateway",
       script: "C:/Users/ZeejMaleesha/.jdks/openjdk-23.0.1/bin/java.exe",
       args: "-jar G:/eca/platform/api-gateway/target/Api-Gateway-1.0.0.jar",
+      log_file: "G:/eca/platform/logs/api-gateway.log",
 
 
     }
